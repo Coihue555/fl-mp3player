753d76ca7aa581f4bcdd8f496edf3bcee0a793da
Coihue555 <kevlar555@gmail.com>
2022-03-29 10:50:00 -0300

    done

--- mp3_player/lib/main.dart
+++ mp3_player/lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:mp3_player/src/models/audioplayer_model.dart';
 import 'package:mp3_player/src/pages/music_player_page.dart';
 import 'package:mp3_player/src/theme/theme.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_)=> new AudioPlayerModel())
+      ],
+     child: MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Music Player',
       theme: miTema,
       home: MusicPlayerScreen()
-    );
+    ));
   }
 }
